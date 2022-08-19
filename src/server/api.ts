c9caa535a083c7bc1c6a67aec3b66d754db435c3
EricChukoltyansky <kazohelmet@gmail.com>
2022-08-19 10:02:17 +0300

    saving tasks to data base

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,3 +1,16 @@
-import { remultExpress } from 'remult/remult-express';
+import { remultExpress } from "remult/remult-express";
+import { Task } from "../shared/Task";
 
-export const api = remultExpress();
+export const api = remultExpress({
+  entities: [Task],
+  initApi: async (remult: any) => {
+    const taskRepo = remult.repo(Task);
+    if ((await taskRepo.count()) === 0) {
+      await taskRepo.insert([
+        { title: "Buy milk", completed: false },
+        { title: "Buy eggs", completed: false },
+        { title: "Buy bread", completed: false },
+      ]);
+    }
+  },
+});
