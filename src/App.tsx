8bf22173bc5fbb57cceb09d78ea741328ebdf601
EricChukoltyansky <kazohelmet@gmail.com>
2022-08-19 10:29:59 +0300

    adding save and add options

--- src/App.tsx
+++ src/App.tsx
@@ -20,14 +20,45 @@ function App() {
   useEffect(() => {
     fetchTasks(hideCompleted).then(setTasks);
   }, [hideCompleted]);
+
+  const addTask = () => {
+    setTasks([...tasks, new Task()]);
+  };
   return (
     <div>
-      {tasks.map((task) => (
-        <div key={task.id}>
-          <input type="checkbox" checked={task.completed} />
-          {task.title}
-        </div>
-      ))}
+      <input
+        type="checkbox"
+        checked={hideCompleted}
+        onChange={(e) => setHideCompleted(e.target.checked)}
+      />
+      Hide Completed
+      <hr />
+      {tasks.map((task) => {
+        const handleChange = (values: Partial<Task>) => {
+          setTasks(tasks.map((t) => (t === task ? { ...task, ...values } : t)));
+        };
+
+        const saveTask = async () => {
+          const savedTask = await taskRepo.save(task);
+          setTasks(tasks.map((t) => (t === task ? savedTask : t)));
+        };
+
+        return (
+          <div key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={(e) => handleChange({ completed: e.target.checked })}
+            />
+            <input
+              value={task.title}
+              onChange={(e) => handleChange({ title: e.target.value })}
+            />
+            <button onClick={saveTask}>Save</button>
+          </div>
+        );
+      })}
+      <button onClick={addTask}>Add Task</button>
     </div>
   );
 }
